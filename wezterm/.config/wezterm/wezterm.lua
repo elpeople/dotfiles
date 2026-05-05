7c82f41132953aef7e65582867c481e9f901eafa
elpeople <elpeoplegm@gmail.com>
2026-05-05 21:48:29 +0900

    wezterm color theme py

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ config.use_ime = true
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 5
+config.color_scheme = "Catppuccin Mocha"
 
 ----------------------------------------------------
 -- Tab
@@ -52,12 +53,12 @@ local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
+  local background = "#313244" -- Surface0
+  local foreground = "#cdd6f4" -- Text
   local edge_background = "none"
   if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
+    background = "#af875f" -- ranger directory brown (ANSI 137)
+    foreground = "#1e1e2e" -- Base (文字を暗くして読みやすくします)
   end
   local edge_foreground = background
   local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
