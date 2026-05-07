6ba0e7b3f7d25d7da6dc2e8dcae79ef8ed7ed7f9
elpeople <elpeoplegm@gmail.com>
2026-05-07 22:17:17 +0900

    fix: cleanup submodules state and restore ranger colorscheme

--- catppuccin/.config/fzf/catppuccin-mocha.sh
+++ catppuccin/.config/fzf/catppuccin-mocha.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Catppuccin Mocha theme for fzf
+# DEBUG: echo "FZF Theme Loading..."
 
 export FZF_DEFAULT_OPTS="
-  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
-  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#89b4fa,hl:#f9e2af
+  --color=fg:#ffffff,header:#f38ba8,info:#cba6f7,pointer:#cba6f7
+  --color=marker:#89b4fa,fg+:#ffffff,prompt:#a6e3a1,hl+:#f9e2af
   --border=rounded
   --border-label=' fzf '
   --preview-window=border-rounded
