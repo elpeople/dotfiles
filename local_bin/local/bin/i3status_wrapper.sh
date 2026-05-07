3fc6bda6e8ee96830a74040659358f41ecfce8bd
elpeople <elpeoplegm@gmail.com>
2026-05-07 23:42:31 +0900

    fix: use relative path in i3status_wrapper.sh for better portability

--- local_bin/local/bin/i3status_wrapper.sh
+++ local_bin/local/bin/i3status_wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# スクリプトがあるディレクトリを取得
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 i3status | while :; do
     read line
-    bluetooth_status=$(/home/elpeople/dotfiles/local_bin/local/bin/bluetooth_status.sh)
+    bluetooth_status="$($SCRIPT_DIR/bluetooth_status.sh)"
     echo "BT: $bluetooth_status | $line"
 done
